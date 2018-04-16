5ae42050fdc6397df7462ea55b33753d14133898
lphan21 <lphan21@csc.com>
2018-04-16 13:21:05 +0700

    add explanation

--- Spring core/sample-autowire-bytype/src/main/java/autowire_bytype/MainApp.java
+++ Spring core/sample-autowire-bytype/src/main/java/autowire_bytype/MainApp.java
@@ -12,3 +12,10 @@ public class MainApp {
 		obj.printBalance("12345678");
 	}
 }
+
+/**
+ * Note:
+ * Autowire by Type: If a bean of class Cat exposes a "dog" property, Spring will find if there are any exactly one bean type "dog" in container (class)
+ 	If yes, Spring will set this bean to the "dog" property. However, if define more than one bean with the same type, a fatal error is raised, and you cannot use byType autowiring for that bean.
+ *
+ */
